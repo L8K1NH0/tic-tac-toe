bf80de0c369dda879867d1bdf25e14648806ceff
Wallace F3lipe <felipewallace489@gmail.com>
2023-11-16 22:03:00 -0300

    Jogo rodando no terminal, e com modo -1 vs 1-

---
+++ teste.py
@@ -0,0 +1,8 @@
+posicao = [ 1, 2, 3,
+            4, "b", 6,
+            7, 8, 9]
+
+
+for num in posicao : #Verificar se chegou ao final == velha
+        if( type(num) == int ):
+                print(f'numero {num}')
